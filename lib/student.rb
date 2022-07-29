629f853db2a1b98cc505b91d51652ce4c86fc8d7
Sandrine <sandrine.thominet@gmail.com>
2022-07-29 16:09:09 -0400

    commit tree 82f6a6841b9ab2cf7ab56064e8bbf33eec878bd7

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+    # Individual students should initialize with an instance variable @knowledge that points to an empty array
+    def initialize
+        @knowledge = []
+    end
+
+    # A method that takes in a string and adds that string to the student's knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge 
+        @knowledge
+    end
 
 end
