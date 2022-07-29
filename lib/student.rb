2319d87fbfbb5b43420964d80096d90495b2d40f
Sandrine <sandrine.thominet@gmail.com>
2022-07-29 16:15:58 -0400

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
+
+    # Individual students should initialize with an instance variable @knowledge that points to an empty array
+    def initialize
+        @knowledge = []
+    end
+
+    # A method that takes in a string and adds that string to the student's knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+
 
 end
